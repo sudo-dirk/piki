cd3335a30acefc434a8b9f587f73a3a313ff783e
Dirk Alders <dirk@mount-mockery.de>
2024-10-06 18:18:11 +0200

    config removed and example added

---
+++ config_example/config.py
@@ -0,0 +1,26 @@
+#
+# Piki Setting
+#
+STARTPAGE = "startpage"
+
+
+#
+# General settings
+#
+# APP_NAME is used for logging
+APP_NAME = 'piki'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: don't run with a dummy secret in production!
+SECRET_KEY = None
+
+# SECURITY WARNING: don't run with '0.0.0.0' in in production, unless you know what you are doing!
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
+
+#
+# Style settings
+#
+DEFAULT_THEME = 'clear-blue'
+
